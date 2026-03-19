6ff1bf56a91bc806ba5560c89b7c95629af85ddd
guestuser <guestuser@localhost>
2026-03-18 18:37:19 -0700

    created the method for 3d array and fixed  technical issues.

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -1,30 +1,26 @@
 import java.io.File;
+import java.util.Stack;
 import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Runner {
+public class Runner{
 
-    public static void main(String[] args) {
 
-        Queue<String> mazeText = getText("Easy_Map_2");
-
-        while (!mazeText.isEmpty()) {
-            System.out.println(mazeText.poll());
-        }
-
-        System.out.println("");
-
-        String[][] mazeGrid = getCords("Easy_Map_Coordinates");
-
-        for (int r = 0; r < mazeGrid.length; r++) {
-            for (int c = 0; c < mazeGrid[0].length; c++) {
-                System.out.print(mazeGrid[r][c]);
+public static void main(String[] args) {
+    
+    String[][][] maze = getText1("src/easymaze1");
+    for(int level = 0; level < maze.length; level++) {
+        for(int i = 0; i < maze[level].length; i++) {
+            for(int j = 0; j < maze[level][0].length; j++) {
+                System.out.print(maze[level][i][j]);
             }
             System.out.println();
         }
     }
+}
+    
 
     public static Queue<String> getText(String filename) {
 
@@ -54,6 +50,44 @@ public class Runner {
 
         return textBased;
     }
+    
+    public static String[][][] getText1(String filename) {
+
+        File fileObj = new File(filename);
+
+        try {
+            Scanner sc = new Scanner(fileObj);
+
+            int rows = Integer.parseInt(sc.next());
+            int cols = Integer.parseInt(sc.next());
+            int numMazes = Integer.parseInt(sc.next());
+            String[][][] maze1 = new String[numMazes][rows][cols];
+
+            int currentRow = 0;
+            int currentLevel = 0;
+            while (sc.hasNext()) {
+                String line = sc.next();
+                
+                for (int col = 0; col < line.length(); col++) {
+                    maze1[currentLevel][currentRow][col] = String.valueOf(line.charAt(col));
+                }
+
+                currentRow++;
+
+                if (currentRow == rows) {
+                    currentRow = 0;
+                    currentLevel++;
+                }
+            }
+
+            return maze1;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 
     public static String[][] getCords(String filename) {
 
